f84bb5496452287ccd2ca9e308f429c9c468af55
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2023-06-20 10:46:51 +0100

    chore: removed old scripts

--- charts/deploy-infra-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd cf-explorer-infra && helm dependency update && cd ..
-
-helm upgrade --install -f cf-explorer-infra/values-dev.yaml -n cf-explorer cf-explorer-infra cf-explorer-infra
--- charts/deploy-infra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd cf-explorer-infra && helm dependency update && cd ..
-
-helm upgrade --install -n cf-explorer cf-explorer-infra cf-explorer-infra
